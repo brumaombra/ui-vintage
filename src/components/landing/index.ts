50d7bc68727a8d8670f725380149d7cd03e26c0f
Mauro Brambilla <maurobra97@gmail.com>
2026-04-13 19:18:34 +0200

    feat: add LandingShell component with customizable layout and update exports

--- src/components/landing/index.ts
+++ src/components/landing/index.ts
@@ -1,5 +1,6 @@
 export { default } from "./landing-navbar";
 export { default as LandingContent } from "./landing-content";
+export { default as LandingShell } from "./landing-shell";
 export { default as LandingNavbar } from "./landing-navbar";
 export { default as LandingFooter } from "./landing-footer";
 export type { LandingFooterLink, LandingFooterSection } from "./landing-footer";
