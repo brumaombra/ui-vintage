bd2edd60ad246d3652ceb78c0e76b931c02fb2ed
Mauro Brambilla <maurobra97@gmail.com>
2026-04-22 19:17:48 +0200

    feat: add Table component and related subcomponents for enhanced table functionality

---
+++ src/components/ui/table/TableCell.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from "vue"
+import { cn } from "@/lib/utils"
+
+const props = defineProps<{
+  class?: HTMLAttributes["class"]
+}>()
+</script>
+
+<template>
+  <td
+    data-slot="table-cell"
+    :class="
+      cn(
+        'px-2 py-3 max-w-[300px] text-xs md:text-sm align-middle whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]',
+        props.class,
+      )
+    "
+  >
+    <slot />
+  </td>
+</template>
